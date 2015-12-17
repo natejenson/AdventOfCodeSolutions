0d072f473c8c3a25f26f2ee37b900987622ddd5a
Nate Jenson <nate.m.jenson@gmail.com>
2015-12-17 00:00:03 -0600

    Day 9, Part Two
    Finish Day 9, part two. Refactor the methods of the first solution to reuse
    more code for the second solution.

--- day9-routes.py
+++ day9-routes.py
@@ -20,6 +20,16 @@
 ##The shortest of these is London -> Dublin -> Belfast = 605, and so the answer is 605 in this example.
 ##
 ##What is the distance of the shortest route?
+##
+##--- Part Two ---
+##
+##The next year, just to show off, Santa decides to take the route with the longest distance instead.
+##
+##He can still start and end at any two (different) locations he wants, and he still must visit each location exactly once.
+##
+##For example, given the distances above, the longest route would be 982 via (for example) Dublin -> London -> Belfast.
+##
+##What is the distance of the longest route?
 
 class Vertex:
     def __init__(self, key):
@@ -53,7 +63,9 @@ class Graph:
         
     def getVertices(self):
         return self.vertices;
-    
+
+
+
 santasMap = Graph()
 
 def fillGraph(lines):
@@ -63,23 +75,34 @@ def fillGraph(lines):
         santasMap.addEdge(cityA,cityB,int(distance))
     
 def determineShortestPath():
-    minDistance = float("inf")
+    return determineBestPath(min)
+
+def determineLongestPath():
+    return determineBestPath(max)
+
+## minOrMax(function): The built in min() or max() function. min()--> shortest path, max()--> longest path.
+def determineBestPath(minOrMax):
+    if (minOrMax == max):
+        bestDistance = 0
+    else:
+        bestDistance = float("inf")
+
     cities = santasMap.getVertices().values()
     for city in cities:
         startCityName = city.getKey()
-        minDistance = min(minDistance, minTravelFromCity(city, 0, [startCityName]))
-    return minDistance
+        bestDistance = minOrMax(bestDistance, travelFromCity(city, 0, [startCityName], minOrMax))
+    return bestDistance
 
 # Side-note: I have such a love-hate relationship with recursion...
-
 ## startCity(Vertex), distanceSoFar(int), visitedCities(list-of-strings)
-def minTravelFromCity(startCity, distanceSoFar, visitedCities):
+## minOrMax(function): The built in min() or max() function. min()--> shortest path, max()--> longest path.
+def travelFromCity(startCity, distanceSoFar, visitedCities, minOrMax):
     if (len(visitedCities) == len(santasMap.getVertices())):
         # We've been around the world! Report back the distance...
         return distanceSoFar
     
     neighbors = startCity.getNeighbors()
-    minTravelFromNeighbors = []
+    travelFromNeighbors = []
     for city in neighbors:
         if (city.getKey() in visitedCities):
             continue
@@ -87,10 +110,11 @@ def minTravelFromCity(startCity, distanceSoFar, visitedCities):
         # Create a new copy of the visited cities, other wise we'll pass the current one in by reference.
         newVisited = visitedCities[:]
         newVisited.append(city.getKey())
-        minTravelFromNeighbors.append(minTravelFromCity(city,distanceSoFar + city.getWeight(startCity),newVisited))
+        travelFromNeighbors.append(travelFromCity(city,distanceSoFar + city.getWeight(startCity),newVisited, minOrMax))
         
-    return min(minTravelFromNeighbors)
-        
+    return minOrMax(travelFromNeighbors)
+
+
 def printGraph():
     for vertex in santasMap.getVertices().values():
         for neighbor in vertex.getNeighbors():
@@ -103,3 +127,4 @@ fillGraph(f.readlines())
 
 # printGraph()
 print("Part One:", determineShortestPath())
+print("Part Two:", determineLongestPath())
